18ff3f9e09f7adbb87ff4430dacdee25592588eb
Manikandan <mtiruman@buffalo.edu>
2015-12-12 23:30:13 -0500

    Figured out the Mongean shuffle

--- cards.py
+++ cards.py
@@ -6,30 +6,14 @@ def homogenous(decks,n):
             ishomo = False
     return ishomo
 
-print "What is the size of the deck?"
-k = input()
-deck = [[1,0] for i in range(0,k)]
-#print deck
+def cards(k):
+    deck = [[1,0] for i in range(0,k)]
+    #print deck
 
-num_step = 0
+    num_step = 0
 
-half1 = deck[:k/2]
-half2 = deck[k/2:]
-for i in range(0,len(half2)):
-    temp = half2[i][0]
-    half2[i][0] = half2[i][1]
-    half2[i][1] = temp
-half2.reverse()
-deck[::2] = half2
-deck[1::2] = half1
-#print deck
-num_step += 1
-
-while (homogenous(deck,k) == False):
     half1 = deck[:k/2]
-    #print half1
     half2 = deck[k/2:]
-    #print half2
     for i in range(0,len(half2)):
         temp = half2[i][0]
         half2[i][0] = half2[i][1]
@@ -37,7 +21,25 @@ while (homogenous(deck,k) == False):
     half2.reverse()
     deck[::2] = half2
     deck[1::2] = half1
-    num_step += 1
     #print deck
+    num_step += 1
 
-print num_step
+    while (homogenous(deck,k) == False):
+        half1 = deck[:k/2]
+        #print half1
+        half2 = deck[k/2:]
+        #print half2
+        for i in range(0,len(half2)):
+            temp = half2[i][0]
+            half2[i][0] = half2[i][1]
+            half2[i][1] = temp
+        half2.reverse()
+        deck[::2] = half2
+        deck[1::2] = half1
+        num_step += 1
+        #print deck
+
+    return num_step
+
+for i in range(2, 53, 2):
+    print str(cards(i)) + ","
