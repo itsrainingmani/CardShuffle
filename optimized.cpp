8c5b77cddb784b08a96813081ab37226697730ca
Manikandan <mtiruman@buffalo.edu>
2016-02-18 14:43:16 -0500

    Adding optimized code

---
+++ optimized.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+bool homogenous(vector<vector<int> v, int n){
+
+}
+
+void cards(int k){
+	
+}
+
+int main(int argc, char const *argv[])
+{
+	cout<<"hello world"<<endl;
+	return 0;
+}
